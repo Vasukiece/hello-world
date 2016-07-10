28e95ec639101bf53b246644cebd8131c2ee4c91
Vasukiece <vasuki1ece2013@gmail.com>
2016-07-10 12:12:02 +0530

    finish

---
+++ pair.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,i,j=1;
+printf("Enter the n values:");
+scanf("%d",&a);
+printf("Enter the pair:");
+scanf("%d",&c);
+for(i=0;i<2*c;i++)
+{
+b=2*a-i;
+if(c!=b)
+{
+printf("The pair is %d\n",c,j++,c);
+}
+}
+return 0;
+}
